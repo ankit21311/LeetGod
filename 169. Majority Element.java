eeaa5c23714d3d8ec5d73c7cdb3fc3c64b0b0e09
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-06-17 20:59:41 +0530

    Update 169. Majority Element.java

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -36,3 +36,36 @@ class Solution {
         
     }
 }
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele = Integer.MIN_VALUE;
+        int count = 0 ;
+        int n = nums.length ;
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i] == ele){
+                count++;
+            }
+            else if(count == 0){
+                ele = nums[i];
+                count++ ;
+            }
+            else{
+                count-- ;
+            }
+        }
+        // Not necessary as it is sure there will be one majority element 
+        // count = 0 ;
+        // for(int i = 0 ; i<n ; i++){
+        //     if(ele == nums[i]){
+        //         count++ ;
+        //     }
+        // }
+        // if(count>n/2){
+        //         return ele ;
+        //     }
+        // return -1 ;
+        return ele ;
+    }
+}
