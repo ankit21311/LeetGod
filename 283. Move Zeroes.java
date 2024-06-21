cb966f1ed8043d2c23c895922c5a63f6049df246
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-06-21 18:50:29 +0530

    Update 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -22,22 +22,18 @@ class Solution {
 
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int nonZeroPos = 0; // Pointer to track the position of the next non-zero element
 
-        // Traverse the array
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                // Swap only if i is not the same as nonZeroPos to avoid unnecessary self-swaps
-                if (i != nonZeroPos) {
-                    int temp = nums[i];
-                    nums[i] = nums[nonZeroPos];
-                    nums[nonZeroPos] = temp;
-                }
-                nonZeroPos++;
+        int nonzero = 0 ;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i] != 0){
+                int temp = nums[nonzero] ;
+                nums[nonzero] = nums[i] ;
+                nums[i] = temp ;
+                nonzero++ ;
             }
         }
-
-        // No need to fill zeroes explicitly, as they are already in place after the swaps
+        
     }
 }
+
+
