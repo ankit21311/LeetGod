cc8668dee96c9b55a1900b7f5f9b3d3d0c19341b
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-06-22 14:53:41 +0530

    Update 918. Maximum Sum Circular Subarray.java

--- 918. Maximum Sum Circular Subarray.java
+++ 918. Maximum Sum Circular Subarray.java
@@ -24,10 +24,7 @@ class Solution {
     }
     return mn;
 }
-
-
-
-    public int maxSubarraySumCircular(int[] nums) {
+public int maxSubarraySumCircular(int[] nums) {
        
        // Straight Max Ans ;
         int ans1 = Kadane(nums);
@@ -43,5 +40,37 @@ class Solution {
             return ans1;
         }
         return Math.max(ans1,ans2) ;
+    }
+}
+
+
+
+//Optimal Approach
+
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        
+        int maxsum = 0 ;
+        int minsum = 0 ;
+        int mx = Integer.MIN_VALUE ;
+        int mn = Integer.MAX_VALUE ;
+        int totalsum = 0 ;
+        for(int i : nums){
+            maxsum += i ;
+            mx = Math.max(mx,maxsum) ;
+            if(maxsum < 0){
+                maxsum = 0 ;
+            }
+            minsum += i ;
+            mn = Math.min(mn,minsum) ;
+            if(minsum > 0){
+                minsum = 0 ;
+            }
+            totalsum += i ;
+        }
+        if(mn == totalsum){
+            return mx ;
+        }
+        return Math.max(mx,totalsum-mn) ;
     }
 }
