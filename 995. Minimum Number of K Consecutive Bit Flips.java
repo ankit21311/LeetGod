1eeaa3ee74450d117205b8da120a445c87c9ba2a
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-06-24 15:17:35 +0530

    Update 995. Minimum Number of K Consecutive Bit Flips.java

--- 995. Minimum Number of K Consecutive Bit Flips.java
+++ 995. Minimum Number of K Consecutive Bit Flips.java
@@ -24,3 +24,32 @@ class Solution {
         return flips ;
     }
 }
+
+// Approach 2
+
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+
+        int flips = 0 ;
+
+    
+        int flipCount = 0 ;
+        for(int i = 0 ; i<nums.length; i++){
+            if(i >= k && nums[i-k] == 2){
+                flipCount-- ;
+            }
+            if(flipCount % 2 == nums[i]){
+
+                if(i+k > nums.length){
+                    return -1 ;
+                }
+                flipCount++ ;
+                flips++ ;
+                nums[i] = 2 ;
+            }
+        }
+
+
+        return flips ;
+    }
+}
