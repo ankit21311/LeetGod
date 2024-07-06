73896d5558645cefba699b1685bb63c2462726ec
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-07-06 14:54:43 +0530

    Update 2582. Pass the Pillow.java

--- 2582. Pass the Pillow.java
+++ 2582. Pass the Pillow.java
@@ -1,3 +1,4 @@
+//Approach 1
 class Solution {
     public int passThePillow(int n, int time) {
         boolean forward = true ;
@@ -14,6 +15,25 @@ class Solution {
                 if(count == 1){
                     forward = true ;
                 }
+            }
+            time-- ;
+        }
+        return count ;
+    }
+}
+
+// Approach 2
+class Solution {
+    public int passThePillow(int n, int time) {
+        int count = 1 ;
+        int dir = 1 ;
+        while(time > 0){
+            if(count+dir>=1 && count+dir <=n){
+                count = count + dir ;
+            }
+            else{
+                dir = dir*(-1) ;
+                count = count + dir;
             }
             time-- ;
         }
