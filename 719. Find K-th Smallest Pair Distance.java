2b6a6caa6c605e875c581df231318fafa63ba018
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2024-08-14 18:52:04 +0530

    Update 719. Find K-th Smallest Pair Distance.java

--- 719. Find K-th Smallest Pair Distance.java
+++ 719. Find K-th Smallest Pair Distance.java
@@ -24,3 +24,41 @@ class Solution {
     return -1 ;
 }
 }
+
+// Approach 2 
+class Solution {
+    public int SlideWindownCount(int[] nums,int D){
+
+        int i = 0 ;
+        int j = 1 ;
+        int n = nums.length ;
+        int PairCount = 0 ;
+        while(j<n){
+            while(nums[j]-nums[i] > D){
+                i++ ;
+            }
+            PairCount += (j-i) ;
+            j++ ;
+        }
+        return PairCount ;
+    }
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums) ;
+        int n = nums.length ;
+        int l = 0 ;
+        int r = nums[n-1]-nums[0] ;
+        int result = 0 ;
+        while(l<=r){
+            int mid = l+(r-l)/2 ;
+            int PairCount = SlideWindownCount(nums,mid) ;
+            if(PairCount < k){
+                l = mid+1 ;
+            }
+            else{
+                result = mid ;
+                r = mid-1 ;
+            }
+        }
+        return result ;
+    }
+}
